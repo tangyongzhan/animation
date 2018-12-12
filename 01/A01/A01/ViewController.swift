8576c850cd25f43b6067f1bf79957a8de455b61d
yongzhan <516855237@qq.com>
2018-12-12 21:12:33 +0700

    facbook 点赞动画

--- 01/A01/A01/ViewController.swift
+++ 01/A01/A01/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //创建背景图view
+    let bgImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "x1"))
+        return imageView
+    }()
+    
+    //创建动画view
+    let iconsContainerView: UIView = {
+       let container = UIView()
+        container.backgroundColor = .red
+        container.frame = CGRect(x: 0, y: 0, width: 240, height: 80)
+        return container
+    }()
+    
+    //隐藏状态条
+    override var prefersStatusBarHidden: Bool { return true }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(bgImageView)
+        bgImageView.frame = view.frame
+        
+        setupLongPressGesture()
     }
-
+    
+    //添加长按事件
+    fileprivate func setupLongPressGesture(){
+        view.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress)))
+    }
+    
+    @objc func handleLongPress(gesture: UILongPressGestureRecognizer){
+        // print("Long pressed!", Date())
+        
+        //长按开始
+        if gesture.state == .began {
+            view.addSubview(iconsContainerView)
+            
+            //获取点击的区域,坐标信息
+            let location = gesture.location(in: self.view)
+            
+            
+            //计算元素在中心位置的x偏移值
+            let centerX = (view.frame.width - iconsContainerView.frame.width) / 2
+            iconsContainerView.transform = CGAffineTransform(translationX: centerX, y: location.y)
+            iconsContainerView.alpha = 0
+            
+            //添加动画
+            UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                //动画信息
+                self.iconsContainerView.alpha = 1
+                self.iconsContainerView.transform = CGAffineTransform(translationX: centerX, y: location.y - self.iconsContainerView.frame.height)
+                
+            })
+            
+        }
+        //长按借宿
+        else if gesture.state == .ended {
+            iconsContainerView.removeFromSuperview()
+        }
+    }
+    
 
 }
 
